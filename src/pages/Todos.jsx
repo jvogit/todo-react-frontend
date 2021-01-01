13dca457e0e8d3e3ae8164dbf4d77ec95f967092
jvogit <justinxvopro@gmail.com>
2021-01-01 04:26:03 -0800

    add basic todo items

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,9 +1,16 @@
 import CenterLayout from "components/layouts/CenterLayout";
 import React, { useEffect, useState } from "react";
 import { Grid, Cell, ALIGNMENT } from 'baseui/layout-grid';
+import { ListItem, ListItemLabel } from "baseui/list";
 import { connect } from "react-redux";
 import ProfileItem from "components/profile/ProfileItem";
 import { getWithToken } from "utils/Request";
+import { H1 } from "baseui/typography";
+import { Button } from "baseui/button";
+import {
+  Checkbox,
+  LABEL_PLACEMENT
+} from "baseui/checkbox";
 
 const TodoList = () => {
   const [data, setData] = useState({});
@@ -17,10 +24,37 @@ const TodoList = () => {
       setData(response);
     })
   }, []);
+
+  return (
+    <ul
+      style={{ padding: "0" }}
+    >
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </ul>
+  )
 }
 
 const TodoItem = ({  }) => {
-
+  return (
+    <ListItem>
+        <div style={{
+          display: "flex",
+          width: "100%",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}>
+          <Checkbox />
+          <div>
+            Test
+          </div>
+          <Button />
+        </div>
+    </ListItem>
+  )
 }
 
 const Todos = ({ user, error, inProgress }) => {
@@ -35,11 +69,20 @@ const Todos = ({ user, error, inProgress }) => {
         <Cell span={[4, 8, 4]}>
           <ProfileItem user={user} />
         </Cell>
-        <Cell span={[4, 8, 4]}>
+        <Cell span={[4, 8, 8]}>
           <div style={{
+            marginTop: "20px",
             width: "100%",
           }}>
-            Yes
+            <div style={{
+              textAlign: "center",
+              alignSelf: "center",
+            }}>
+              <H1>Todos</H1>
+            </div>
+            <div>
+              <TodoList />
+            </div>
           </div>
         </Cell>
       </Grid>
