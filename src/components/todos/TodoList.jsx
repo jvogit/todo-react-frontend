c7dea3b0d552310b7286c51af36b4bdede9a9b30
jvogit <justinxvopro@gmail.com>
2021-01-07 01:01:35 -0800

    add uneditable list

--- src/components/todos/TodoList.jsx
+++ src/components/todos/TodoList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, } from "react";
-import { arrayMove, arrayRemove, List } from "baseui/dnd-list";
+import { ListItem } from "baseui/list";
+import { arrayMove, arrayRemove, List, StyledLabel } from "baseui/dnd-list";
 import { getWithToken, postWithToken, requestWithToken } from "utils/Request";
 import { HeadingSmall } from "baseui/typography";
 import { StyledSpinnerNext as Spinner } from "baseui/spinner";
@@ -7,7 +8,7 @@ import TodoItem from "./TodoItem";
 import { Button, KIND } from "baseui/button";
 import { Plus } from "baseui/icon";
 
-const TodoList = ({ date = "2021-01-01" }) => {
+const TodoList = ({ date = "2021-01-01", editing = false }) => {
   const [loading, setLoading] = useState(true);
   const [items, setItems] = useState([]);
 
@@ -64,7 +65,7 @@ const TodoList = ({ date = "2021-01-01" }) => {
       ? arrayRemove(items, old_index, new_index)
       : arrayMove(items, old_index, new_index);
 
-    return new_array.map((item, index) => ({...item, index}));
+    return new_array.map((item, index) => ({ ...item, index }));
   }
 
   if (loading) {
@@ -87,6 +88,26 @@ const TodoList = ({ date = "2021-01-01" }) => {
         <HeadingSmall>Loading</HeadingSmall>
       </div>
     )
+  }
+
+  if (!editing) {
+    return (
+      <ul
+        style={{
+          padding: "0"
+        }}
+      >
+        {
+          mapToComponent(items).map((item, index) => (
+            <ListItem key={index} >
+              <StyledLabel>
+                {item}
+              </StyledLabel>
+            </ListItem>
+          ))
+        }
+      </ul>
+    );
   }
 
   return (
--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Cell, } from 'baseui/layout-grid';
 import { connect } from "react-redux";
 import ProfileItem from "components/profile/ProfileItem";
-import { H1, } from "baseui/typography";
+import { HeadingSmall, } from "baseui/typography";
 import TodoList from "components/todos/TodoList";
+import { Button, KIND } from "baseui/button";
 
 const Todos = ({ user, error, inProgress }) => {
+  const [editing, setEditing] = useState(false);
 
   if (inProgress || error) {
     return null;
@@ -22,12 +24,21 @@ const Todos = ({ user, error, inProgress }) => {
           width: "100%",
         }}>
           <div style={{
-            textAlign: "center",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
           }}>
-            <H1>Todos</H1>
+            <HeadingSmall>01-01-2021 TODO</HeadingSmall>
+            <Button 
+              kind={KIND.minimal}
+              onClick={() => setEditing(!editing)}
+            >
+              Edit
+            </Button>
           </div>
           <div>
-            <TodoList />
+            <TodoList editing={editing} />
           </div>
         </div>
       </Cell>
